5eed37091a6b2fa7a3e3afba50d2fa5e4e5dbc15
James Huang <jameshuang304@gmail.com>
2017-06-25 00:13:13 +0800

    Initial commit

---
+++ src/com/oopfinal/main/ID.java
@@ -0,0 +1,13 @@
+package com.oopfinal.main;
+
+
+public enum ID {
+    Player(),
+    //Player2(),
+    BasicEnemy(),
+    FastEnemy(),
+    SmartEnemy(),
+    EnemyBoss(),
+    MenuParticle(),
+    Trail();
+}
